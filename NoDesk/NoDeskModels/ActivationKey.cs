690f829dcbb8d817ebf718a75a67fdefb6e0b07f
BastiaanvdB <70689873+BastiaanvdB@users.noreply.github.com>
2021-09-21 21:19:47 +0200

    commit 15

--- NoDesk/NoDeskModels/ActivationKey.cs
+++ NoDesk/NoDeskModels/ActivationKey.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace NoDeskModels
 {
     public class ActivationKey
     {
+        [BsonId]
+        public Guid Id { get; set; }
+        [BsonElement("InvitationKeys")]
+        public string[] InvitationKeys { get; set; }
     }
 }
