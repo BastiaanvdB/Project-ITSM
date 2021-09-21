8e680517ee82673d2b74fcd18e46c191ca775076
BastiaanvdB <70689873+BastiaanvdB@users.noreply.github.com>
2021-09-21 10:49:32 +0200

    commit 10

--- NoDesk/NoDeskUI/Login.Designer.cs
+++ NoDesk/NoDeskUI/Login.Designer.cs
@@ -34,13 +34,18 @@ namespace NoDeskUI
             this.TextboxPassword = new System.Windows.Forms.TextBox();
             this.BTNLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TextboxUsername
             // 
             this.TextboxUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.TextboxUsername.Location = new System.Drawing.Point(71, 51);
+            this.TextboxUsername.Location = new System.Drawing.Point(61, 131);
             this.TextboxUsername.Name = "TextboxUsername";
             this.TextboxUsername.Size = new System.Drawing.Size(230, 29);
             this.TextboxUsername.TabIndex = 0;
@@ -48,15 +53,16 @@ namespace NoDeskUI
             // TextboxPassword
             // 
             this.TextboxPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.TextboxPassword.Location = new System.Drawing.Point(71, 98);
+            this.TextboxPassword.Location = new System.Drawing.Point(61, 191);
             this.TextboxPassword.Name = "TextboxPassword";
+            this.TextboxPassword.PasswordChar = '•';
             this.TextboxPassword.Size = new System.Drawing.Size(230, 29);
             this.TextboxPassword.TabIndex = 1;
             // 
             // BTNLogin
             // 
             this.BTNLogin.BackColor = System.Drawing.Color.White;
-            this.BTNLogin.Location = new System.Drawing.Point(133, 157);
+            this.BTNLogin.Location = new System.Drawing.Point(121, 257);
             this.BTNLogin.Name = "BTNLogin";
             this.BTNLogin.Size = new System.Drawing.Size(107, 47);
             this.BTNLogin.TabIndex = 2;
@@ -67,13 +73,71 @@ namespace NoDeskUI
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.panel1.Controls.Add(this.linkLabel2);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.labelLogo);
+            this.panel1.Controls.Add(this.labelPassword);
+            this.panel1.Controls.Add(this.labelUsername);
             this.panel1.Controls.Add(this.TextboxUsername);
             this.panel1.Controls.Add(this.TextboxPassword);
             this.panel1.Controls.Add(this.BTNLogin);
-            this.panel1.Location = new System.Drawing.Point(83, 125);
+            this.panel1.Location = new System.Drawing.Point(97, 90);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(384, 252);
+            this.panel1.Size = new System.Drawing.Size(346, 364);
             this.panel1.TabIndex = 3;
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.linkLabel2.Location = new System.Drawing.Point(121, 223);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(100, 15);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Forgot password?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.linkLabel1.Location = new System.Drawing.Point(221, 223);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(70, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Registration";
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Font = new System.Drawing.Font("Segoe UI", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelLogo.ForeColor = System.Drawing.Color.White;
+            this.labelLogo.Location = new System.Drawing.Point(66, 28);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(225, 71);
+            this.labelLogo.TabIndex = 5;
+            this.labelLogo.Text = "NoDesk";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.ForeColor = System.Drawing.Color.White;
+            this.labelPassword.Location = new System.Drawing.Point(61, 173);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(57, 15);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.ForeColor = System.Drawing.Color.White;
+            this.labelUsername.Location = new System.Drawing.Point(61, 113);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(94, 15);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Username/Email";
             // 
             // Login
             // 
@@ -100,5 +164,10 @@ namespace NoDeskUI
         private System.Windows.Forms.TextBox TextboxPassword;
         private System.Windows.Forms.Button BTNLogin;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
