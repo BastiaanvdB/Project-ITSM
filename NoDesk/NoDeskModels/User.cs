b8bb5b10822be7cff61d994a40ad53a6d237eb89
BastiaanvdB <70689873+BastiaanvdB@users.noreply.github.com>
2021-09-21 11:09:29 +0200

    commit 12

---
+++ NoDesk/NoDeskModels/User.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoDeskModels
+{
+    public class User
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        [BsonElement("Firstname")]
+        public string Firstname { get; set; }
+        [BsonElement("Lastname")]
+        public string Lastname { get; set; }
+        [BsonElement("Email")]
+        public string Email { get; set; }
+        [BsonElement("Password")]
+        public string Password { get; set; }
+        [BsonElement("Role")]
+        public Roles Role { get; set; }
+    }
+}
